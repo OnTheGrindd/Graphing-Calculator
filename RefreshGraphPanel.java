a043d1ffae11ef0077e82bc0f9df9a44b9b81bcd
Joshua Beerel <bikerj15@yahoo.com>
2018-04-03 13:49:18 -0400

    initialized RefreshGraphPanel to receive window params and x/y scale values

--- RefreshGraphPanel.java
+++ RefreshGraphPanel.java
@@ -1,8 +1,66 @@
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class RefreshGraphPanel {
+import javax.swing.JPanel;
 
-	public RefreshGraphPanel() {
+
+public class RefreshGraphPanel extends JPanel implements MouseListener {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	GraphingCalculator calculator;
+	String expr;
+	double[] xComponents;
+	double[] yComponents;
+	
+	public RefreshGraphPanel(GraphingCalculator gc, String expression, double[] xValues, double[] yValues) throws IllegalArgumentException {
 		// TODO Auto-generated constructor stub
+		calculator = gc;
+		expr = expression;
+		yComponents = yValues;
+		xComponents = xValues;
+		this.addMouseListener(this);
+	}
+	
+	@Override
+	public void paint(Graphics g) { // overrides paint() in JPanel 
+		int windowWidth = getWidth();
+		int windowHeight = getHeight();
+		System.out.println("Current graph size is " + windowWidth + " x " + windowHeight);
+		// paint the graph
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
